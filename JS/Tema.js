8f0db5a2512cbf436dccf051c7980a7766d7b3d3
victorhmrod <vhcgames1@gmail.com>
2024-06-17 20:22:29 -0300

    Initial commit

---
+++ JS/Tema.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", function () {
+  body.classList.toggle("light-mode");
+});
+
+
+
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', function() {
+  const icon = themeToggle.querySelector('ion-icon');
+  if (icon.getAttribute('name') === 'sunny-outline') {
+    icon.setAttribute('name', 'moon-outline');
+  } else {
+    icon.setAttribute('name', 'sunny-outline');
+  }
+});
+
+
+
